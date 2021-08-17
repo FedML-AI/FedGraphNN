8e3193590ea7e814399607f3fe43981e3ee7d0bc
Oxfordblue7 <457620544@qq.com>
2021-08-17 12:48:49 -0400

    Updated the data loader.

--- data_preprocessing/subgraph_level/data_loader.py
+++ data_preprocessing/subgraph_level/data_loader.py
@@ -13,108 +13,24 @@ import seaborn as sns
 import torch
 from torch_geometric.datasets import CitationFull
 from torch_geometric.data import Data, DataLoader
-from torch_geometric.utils import k_hop_subgraph, from_networkx
+from torch_geometric.utils import from_networkx
+
 
 from FedML.fedml_core.non_iid_partition.noniid_partition import partition_class_samples_with_dirichlet_distribution
 
 
-def _subgraphing(g, partion):
-    nodelist = [None] * len(set(partion.values()))
-    for k, v in partion.items():
-        if nodelist[v] is None:
-            nodelist[v] = []
-        nodelist[v].append(k)
+def get_data_community(path, data, pred_task):
+    if pred_task == 'relType_prediction' && algo == 'Louvain':
+    # for relation type prediction
+        subdir = 'subgraphs_byLouvain'
 
-    graphs = []
-    for nodes in nodelist:
-        if len(nodes) < 2:
-            continue
-        graphs.append(from_networkx(nx.subgraph(g, nodes)))
-    return graphs
+    if pred_task == 'link_prediction':
+    # for link prediction with communities grouped by the relation type
+        subdir = 'subgraphs_byRelType'
 
-
-def _read_mapping(path, data, filename):
-    mapping = dict()
-    df = pd.read_csv(os.path.join(path, data, filename), sep='\t', header=None, index_col=None)
-    for _, row in df.iterrows():
-        mapping[row[1]] = int(row[0])
-    # with open(os.path.join(path, data, filename)) as f:
-    #     for line in f:
-    #         s = line.strip().split()
-    #         mapping[s[1]] = int(s[0])
-    
-    return mapping
-
-
-def _build_nxGraph(path, data, filename, mapping_entities, mapping_relations):
-    G = nx.Graph()
-    df = pd.read_csv(os.path.join(path, data, filename), sep='\t', header=None, index_col=None)
-    for _, row in df.iterrows():
-        G.add_edge(mapping_entities[row[0]], mapping_entities[row[2]], edge_label=mapping_relations[row[1]])
-    # with open(os.path.join(path, data, filename)) as f:
-    #     for line in f:
-    #         s = line.strip().split()
-    #         G.add_edge(mapping_entities[s[0]], mapping_entities[s[2]], edge_label=mapping_relations[s[1]])
-    return G
-
-
-def get_data_community(path, data, algo):
-    """ For relation type prediction. """
-
-    mapping_entities = _read_mapping(path, data, 'entities.dict')
-    mapping_relations = _read_mapping(path, data, 'relations.dict')
-
-    g_train = _build_nxGraph(path, data, 'train.txt', mapping_entities, mapping_relations)
-    g_val = _build_nxGraph(path, data, 'valid.txt', mapping_entities, mapping_relations)
-    g_test = _build_nxGraph(path, data, 'test.txt', mapping_entities, mapping_relations)
-
-    assert algo in ['Louvain', 'girvan_newman', 'Clauset-Newman-Moore', 'asyn_lpa_communities', 'label_propagation_communities']
-
-    if algo == 'Louvain':
-        partion = community_louvain.best_partition(g_train)
-        graphs_train = _subgraphing(g_train, partion)
-        partion = community_louvain.best_partition(g_val)
-        graphs_val = _subgraphing(g_val, partion)
-        partion = community_louvain.best_partition(g_test)
-        graphs_test = _subgraphing(g_test, partion)
-
-    # algorithms:
-    # Louvain
-    # girvan_newman
-    # greedy_modularity_communities
-    # asyn_lpa_communities
-    # label_propagation_communities
-
-    return graphs_train, graphs_val, graphs_test
-
-
-def _build_pygGraph(relType, df, mapping_entities, mapping_relations):
-    df[0].replace(mapping_entities, inplace=True)
-    df[1].replace(mapping_relations, inplace=True)
-    df[2].replace(mapping_entities, inplace=True)
-
-    g = nx.Graph()
-    g.add_edges_from(zip(df[0], df[2]), edge_label=mapping_relations[relType])
-    
-    return from_networkx(g)
-
-
-def _build_graphs_by_relType(path, data, filename, mapping_entities, mapping_relations):
-    df = pd.read_csv(os.path.join(path, data, filename), sep='\t', header=None, index_col=None)
-    
-    graphs = [_build_pygGraph(relType, group, mapping_entities, mapping_relations) for relType, group in df.groupby(1)]
-    return graphs
-
-
-def get_data_community_byRelType(path, data):
-    """ For link prediction with communities grouped by the relation type. """
-
-    mapping_entities = _read_mapping(path, data, 'entities.dict')
-    mapping_relations = _read_mapping(path, data, 'relations.dict')
-
-    graphs_train = _build_graphs_by_relType(path, data, 'train.txt', mapping_entities, mapping_relations)
-    graphs_val = _build_graphs_by_relType(path, data, 'valid.txt', mapping_entities, mapping_relations)
-    graphs_test = _build_graphs_by_relType(path, data, 'test.txt', mapping_entities, mapping_relations)
+    graphs_train = pickle.load(open(os.path.join(path, data, subdir, 'train.pkl'), 'rb'))
+    graphs_val = pickle.load(open(os.path.join(path, data, subdir, 'valid.pkl'), 'rb'))
+    graphs_test = pickle.load(open(os.path.join(path, data, subdir, 'test.pkl'), 'rb'))
 
     # number of graphs == number of relation type
     return graphs_train, graphs_val, graphs_test
@@ -123,11 +39,15 @@ def get_data_community_byRelType(path, data):
 def create_random_split(path, data, pred_task='link_pred', algo='Louvain'):
     assert pred_task in ['rel_pred', 'link_pred']
 
+<<<<<<< Updated upstream
     graphs_train, graphs_val, graphs_test = None, None , None
     if pred_task == 'rel_pred':
         graphs_train, graphs_val, graphs_test = get_data_community(path, data, algo)
     if pred_task == 'link_pred':
         graphs_train, graphs_val, graphs_test = get_data_community_byRelType(path, data)
+=======
+    graphs_train, graphs_val, graphs_test = get_data_community(path, data, pred_task)
+>>>>>>> Stashed changes
 
     return graphs_train, graphs_val, graphs_test
 
@@ -311,5 +231,4 @@ def load_partition_data(args, path, client_number, uniform=True, global_test=Tru
 
     return train_data_num, val_data_num, test_data_num, train_data_global, val_data_global, test_data_global, \
            data_local_num_dict, train_data_local_dict, val_data_local_dict, test_data_local_dict
-
 
