7414b7bef74b3f33c8ab32013bae0e602b7d6e95
YangLiangwei <805797986@qq.com>
2021-08-23 13:23:45 -0500

    add recommender system task

--- data_preprocessing/recommender_system/data_loader.py
+++ data_preprocessing/recommender_system/data_loader.py
@@ -18,6 +18,25 @@ from torch_geometric.utils import train_test_split_edges
 
 from FedML.fedml_core.non_iid_partition.noniid_partition import partition_class_samples_with_dirichlet_distribution
 
+def split_graph(graph, train_ratio = 0.8, val_ratio = 0.1, test_ratio = 0.1):
+    assert train_ratio + val_ratio + test_ratio == 1
+    edge_size = graph.edge_label.size()[0]
+    train_num = int(edge_size * train_ratio)
+    val_num = int(edge_size * val_ratio)
+    test_num = edge_size - train_num - val_num
+    
+    [train_split, val_split, test_split] = torch.utils.data.random_split(range(edge_size), [train_num, val_num, test_num])
+    train_split = torch.tensor(train_split)
+    val_split = torch.tensor(val_split)
+    test_split = torch.tensor(test_split)
+    graph.edge_train = graph.edge_index[:, train_split]
+    graph.label_train = graph.edge_label[train_split]
+    graph.edge_val = graph.edge_index[:, val_split]
+    graph.label_val = graph.edge_label[val_split]
+    graph.edge_test = graph.edge_index[:, test_split]
+    graph.label_test = graph.edge_label[test_split]
+    
+    return graph
 
 def _convert_to_nodeDegreeFeatures(graphs):
     graph_infos = []
@@ -92,10 +111,8 @@ def get_data_category(path, data, algo):
     graphs = _convert_to_nodeDegreeFeatures(graphs)
     graphs_split = []
     for g in graphs:
-        graphs_split.append(train_test_split_edges(g, val_ratio = 0.1, test_ratio = 0.1))
-    
+        graphs_split.append(split_graph(g))
     return graphs_split
-    # return graphs_train, graphs_val, graphs_test
 
 def partition_by_category(graph, mapping_item2category):
     partition = {}
@@ -110,9 +127,9 @@ def partition_by_category(graph, mapping_item2category):
 def create_category_split(path, data, pred_task='link_prediction', algo='Louvain'):
     assert pred_task in ['link_prediction']
 
-    graphs_train, graphs_val, graphs_test = get_data_category(path, data, algo)
+    graphs_split = get_data_category(path, data, algo)
 
-    return graphs_train, graphs_val, graphs_test
+    return graphs_split
 
 
 def create_non_uniform_split(args, idxs, client_number, is_train=True):
@@ -145,63 +162,22 @@ def create_non_uniform_split(args, idxs, client_number, is_train=True):
     return idx_batch_per_client
 
 
-def partition_data_by_category(args, path, client_number, uniform=True, compact=True):
-    graphs_train, graphs_val, graphs_test = create_category_split(path, args.dataset, args.pred_task, args.part_algo)
+def partition_data_by_category(args, path, compact=True):
+    graphs_split = create_category_split(path, args.dataset, args.pred_task, args.part_algo)
 
-    num_train_samples = len(graphs_train)
-    num_val_samples = len(graphs_val)
-    num_test_samples = len(graphs_test)
-
-    train_idxs = list(range(num_train_samples))
-    val_idxs = list(range(num_val_samples))
-    test_idxs = list(range(num_test_samples))
-
-    random.shuffle(train_idxs)
-    random.shuffle(val_idxs)
-    random.shuffle(test_idxs)
+    client_number = len(graphs_split)
 
     partition_dicts = [None] * client_number
 
-    if uniform:
-        clients_idxs_train = np.array_split(train_idxs, client_number)
-        clients_idxs_val = np.array_split(val_idxs, client_number)
-        clients_idxs_test = np.array_split(test_idxs, client_number)
-    else:
-        clients_idxs_train = create_non_uniform_split(args, train_idxs, client_number, True)
-        clients_idxs_val = create_non_uniform_split(args, val_idxs, client_number, False)
-        clients_idxs_test = create_non_uniform_split(args, test_idxs, client_number, False)
-
     labels_of_all_clients = []
     for client in range(client_number):
-        client_train_idxs = clients_idxs_train[client]
-        client_val_idxs = clients_idxs_val[client]
-        client_test_idxs = clients_idxs_test[client]
 
-        train_graphs_client = [graphs_train[idx] for idx in client_train_idxs]
-        train_labels_client = [graphs_train[idx].y for idx in client_train_idxs]
-        labels_of_all_clients.append(train_labels_client)
-
-        val_graphs_client = [graphs_val[idx] for idx in client_val_idxs]
-        
-        val_labels_client = [graphs_val[idx].y for idx in client_val_idxs]
-        labels_of_all_clients.append(val_labels_client)
-
-        test_graphs_client = [graphs_test[idx] for idx in client_test_idxs]
-
-        test_labels_client = [graphs_test[idx].y for idx in client_test_idxs]
-        labels_of_all_clients.append(test_labels_client)
-
-
-        partition_dict = {'train': train_graphs_client,
-                          'val': val_graphs_client,
-                          'test': test_graphs_client}
-
+        partition_dict = {'graph': graphs_split[client_number]}
         partition_dicts[client] = partition_dict
 
     global_data_dict = {
-        'train': graphs_train,
-        'val': graphs_val,
-        'test': graphs_test}
+        'graphs': graphs_split,
+        }
 
     return global_data_dict, partition_dicts
 
@@ -213,40 +189,24 @@ def load_partition_data(args, path, client_number, uniform=True, global_test=Tru
 
     data_local_num_dict = dict()
     train_data_local_dict = dict()
-    val_data_local_dict = dict()
-    test_data_local_dict = dict()
 
     collator = WalkForestCollator(normalize_features=normalize_features) if compact \
         else DefaultCollator(normalize_features=normalize_features, normalize_adj=normalize_adj)
 
     # This is a PyG Dataloader
-    train_data_global = DataLoader(global_data_dict['train'], batch_size=args.batch_size, shuffle=True, collate_fn=collator,
+    train_data_global = DataLoader(global_data_dict['graphs'], batch_size=1, shuffle=True, collate_fn=collator,
                                         pin_memory=True)
-    val_data_global = DataLoader(global_data_dict['val'], batch_size=args.batch_size, shuffle=True, collate_fn=collator,
-                                      pin_memory=True)
-    test_data_global =  DataLoader(global_data_dict['test'], batch_size=args.batch_size, shuffle=True, collate_fn=collator,
-                                       pin_memory=True)
 
-    train_data_num = len(global_data_dict['train'])
-    val_data_num = len(global_data_dict['val'])
-    test_data_num = len(global_data_dict['test'])
+    train_data_num = len(global_data_dict['graphs'])
 
     for client in range(client_number):
-        train_dataset_client = partition_dicts[client]['train']
-        val_dataset_client = partition_dicts[client]['val']
-        test_dataset_client = partition_dicts[client]['test']
+        train_dataset_client = partition_dicts[client]['graph']
 
         data_local_num_dict[client] = len(train_dataset_client)
-        train_data_local_dict[client] = DataLoader(train_dataset_client, batch_size=args.batch_size, shuffle=True,
+        train_data_local_dict[client] = DataLoader(train_dataset_client, batch_size=1, shuffle=True,
                                                         collate_fn=collator, pin_memory=True)
-        val_data_local_dict[client] = DataLoader(val_dataset_client, batch_size=args.batch_size, shuffle=False,
-                                                      collate_fn=collator, pin_memory=True)
-        test_data_local_dict[client] = test_data_global if global_test else DataLoader(test_dataset_client,
-                                                                                            batch_size=args.batch_size, shuffle=False,
-                                                                                            collate_fn=collator,
-                                                                                            pin_memory=True)
-
         logging.info("Client idx = {}, local sample number = {}".format(client, len(train_dataset_client)))
 
+    val_data_num = test_data_num = val_data_global = test_data_global = val_data_local_dict = test_data_local_dict = 0
     return train_data_num, val_data_num, test_data_num, train_data_global, val_data_global, test_data_global, \
            data_local_num_dict, train_data_local_dict, val_data_local_dict, test_data_local_dict
