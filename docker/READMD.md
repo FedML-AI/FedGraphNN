f20cba28d7a43fe0f5a952970c87e069872b3711
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-15 14:26:01 -0400

    refactor to industrial-grade FedCV platform with new MLOps and FedML support

--- docker/READMD.md
+++
@@ -1,8 +0,0 @@
-
-
-# Build Docker Image
-```
-# install docker engine (https://docs.docker.com/engine/install/ubuntu/)
-sudo chmod 777 /var/run/docker.sock
-docker build . -f Dockerfile.fedgraphnn
-```
---
+++ moleculenet_graph_clf/config/bootstrap.sh
@@ -0,0 +1,10 @@
+echo ${TORCH}
+echo ${CUDA}
+pip install torch-scatter -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-geometric
+
+DATA_PATH=~/fedgraphnn_data/clintox
+wget --no-check-certificate --no-proxy -P $DATA_PATH https://fedmol.s3-us-west-1.amazonaws.com/datasets/clintox/clintox.zip &&
+cd $DATA_PATH &&
+unzip clintox.zip && rm clintox.zip
