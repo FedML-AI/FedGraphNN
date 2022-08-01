3e76bc82fba8854cf7c529db93e16f5b582e71d6
Chaoyang He <chaoyanghe.com@gmail.com>
2022-08-01 09:14:50 -0700

    merge fedgraphnn into fedml repo

--- moleculenet_graph_clf/run_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python3 fedml_moleculenet_property_prediction.py --run_id fedgraphnn_cs_ch --cf config/fedml_config.yaml --rank 0 --role server
--- moleculenet_graph_reg/run_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python3 fedml_moleculenet_property_prediction.py --cf config/fedml_config.yaml --rank 0 --role server
